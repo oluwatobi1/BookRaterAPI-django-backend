95d0856a41f851a3868b2afe6bf395e93163f43e
oluwatobi1 <akintunlesetobi@gmail.com>
2020-12-12 14:21:20 +0100

    customizing django admin

--- demo/admin.py
+++ demo/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Book
 # Register your models here.
-admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    fields=['title', 'description', 'price']
+    list_display = ['title', 'price']
+    list_filter = ['is_published', 'price']
+    search_fields = ['description']
