74be3c85e0e64c75c6df32571593788edce726a9
oluwatobi1 <akintunlesetobi@gmail.com>
2020-12-12 13:32:25 +0100

    Model objects methods

--- demo/views.py
+++ demo/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from .models import Book
 
 # Create your views here.
+class Another(View):
+    books = Book.objects.get(id=1)
+    out = f'we have {books.title } in database with id {books.id}<br>'
+
+    # for each in books:
+    #     out += f'we have {each.title } in database<br>'
+
+    def get(self, request):
+        return HttpResponse(self.out)
+
 def fun(request):
     return HttpResponse("Hello WOrld")
