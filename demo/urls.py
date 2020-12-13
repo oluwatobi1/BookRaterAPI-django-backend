94568e92513e97cfca3eadf8f3a2fdb4ddb0c86d
oluwatobi1 <akintunlesetobi@gmail.com>
2020-12-13 19:40:37 +0100

    REST and serializers

--- demo/urls.py
+++ demo/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from .views import BookViewSet
 from . import views
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register('book', BookViewSet)
 
 urlpatterns = [
     path('', views.fun),
+    path('s', include(router.urls))
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
 from django.views import View
 from .models import Book
+from rest_framework import viewsets
+from .serializer import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
 
 
 def fun(request):
