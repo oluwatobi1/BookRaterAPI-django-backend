3b387cd1d1a4c9b47944b69cb00723e679946aab
oluwatobi1 <akintunlesetobi@gmail.com>
2020-12-15 17:51:56 +0100

    custom rate_movie api created

--- MovieRaterProject/api/models.py
+++ MovieRaterProject/api/models.py
@@ -2,17 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=26)
-    description = models.TextField(max_length =100)
+    description = models.TextField(max_length=100)
+
     def __str__(self):
         return self.title
 
+
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE) ################ Meaning if youd delet a movie the user goes???
-    stars = models.IntegerField(validators = [MaxValueValidator, MinValueValidator])
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # Meaning if you'd delete a movie the user goes???
+    stars = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
+
     class Meta:
-        unique_together = ['user', 'movie']
+        unique_together = (('user', 'movie'))
         indexes = [
-            models.Index(fields=['user', 'movie']),]
+            models.Index(fields=['user', 'movie']), ]
--- MovieRaterProject/api/views.py
+++ MovieRaterProject/api/views.py
@@ -1,11 +1,42 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from django.contrib.auth.models import User
+
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+    @action(detail=True, methods=['POST'])
+    def rate_movie(self, request, pk=None):
+        if 'stars' in request.data:
+            movie = Movie.objects.get(id=pk)
+            user = User.objects.get(id=1)
+            stars = request.data['stars']
+
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+                serializer= RatingSerializer(rating, many=False)
+                response = {"message": "Rating Updated",
+                            "result": serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+
+            except:
+                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {"message": "Rating Created",
+                            "result": serializer.data}
+                return Response(response, status=status.HTTP_201_CREATED)
+
+        else:
+            response = {"message": "enter 'stars'"}
+            return Response(response, status=status.HTTP_204_NO_CONTENT)
+
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
