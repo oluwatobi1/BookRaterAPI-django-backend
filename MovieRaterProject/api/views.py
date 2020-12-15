b012ee4392d8055f69f0156a4fa61783973bcd4e
oluwatobi1 <akintunlesetobi@gmail.com>
2020-12-15 15:28:04 +0100

    Serializer and views setup

--- MovieRaterProject/api/views.py
+++ MovieRaterProject/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Movie, Rating
+from .serializers import MovieSerializer, RatingSerializer
+from rest_framework.views import viewsets
 
-# Create your views here.
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
