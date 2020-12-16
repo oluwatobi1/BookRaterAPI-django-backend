73b00a9d795bcc04e06650cc4a7162bbea811231
oluwatobi1 <akintunlesetobi@gmail.com>
2020-12-16 14:08:55 +0100

    auth for api calls

--- MovieRaterProject/api/views.py
+++ MovieRaterProject/api/views.py
@@ -1,20 +1,24 @@
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
 from rest_framework import viewsets, status
 from django.contrib.auth.models import User
 
 
+
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    authentication_classes = (TokenAuthentication, )
 
     @action(methods=["POST"], detail=True)
     def rate_movie(self, request, pk=None):
         if 'stars' in request.data:
             movie = Movie.objects.get(id=pk)
-            user = User.objects.get(pk=1)
+            user = request.user
+            print('user ', user)
             stars = request.data['stars']
             try:
                 rating = Rating.objects.get(movie=movie.id, user=user.id)
@@ -26,13 +30,13 @@ class MovieViewSet(viewsets.ModelViewSet):
                 return Response(response, status=status.HTTP_200_OK)
 
             except:
-                rating = Rating.objects.create(movie=movie.id, user=user.id, stars=stars)
+                rating = Rating.objects.create(movie=movie, user=user, stars=stars)
                 serializers = RatingSerializer(rating, many=False)
                 response = {"message": "Created successful",
                             "result": serializers.data}
                 return Response(response, status=status.HTTP_200_OK)
 
-            #do stuff
+
         else:
             response = {"message": "You need to input stars field"}
             return Response(response, status=status.HTTP_200_OK)
@@ -41,3 +45,4 @@ class MovieViewSet(viewsets.ModelViewSet):
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+    authentication_classes = (TokenAuthentication, )
